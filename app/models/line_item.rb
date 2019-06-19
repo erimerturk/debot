4be17cd95843e1dd967efe677f861b98acca504d
erimerturk <erimerturk@gmail.com>
2019-06-19 13:16:54 +0300

    Worked on smart card

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,4 +1,9 @@
 class LineItem < ApplicationRecord
   belongs_to :product
   belongs_to :cart
+  attribute :quantity, :integer, default: -> { 1 }
+
+  def total_price
+    product.price * (quantity || 1)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -11,6 +11,22 @@ Product.create!(
       experienced developer needing to expand your options. Plus, you’ll find
       out which cross-platform solution makes the most sense for your needs.
       </p>},
-   image_url: 'back.jpg',
+   image_url: 'joker-image.png',
    price: 26.00
    )
+
+
+Product.create!(
+    title: 'Joker Book',
+    description:
+        %{<p>
+      <em>Native Apps, Multiple Platforms</em>
+      Answer the question “Can we build this for ALL the devices?” with a
+      resounding YES. This book will help you get there with a real-world
+      introduction to seven platforms, whether you’re new to mobile or an
+      experienced developer needing to expand your options. Plus, you’ll find
+      out which cross-platform solution makes the most sense for your needs.
+      </p>},
+    image_url: 'joker-image.png',
+    price: 12.00
+)
